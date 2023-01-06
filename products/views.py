40fed1289f1261e79a5faa36e60993bfcbb0373e
SiNC <sauronart@gmail.com>
2023-01-06 16:51:53 +0600

    just initial

---
+++ products/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from products.models import Product, ProductCategory
+
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Store',
+    }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': Product.objects.all(),
+        'catefories': ProductCategory.objects.all()
+    }
+    return render(request, 'products/products.html', context)
